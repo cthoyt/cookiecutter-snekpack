b2c8ca9bd8762d058eb7ea02e66b67a8f1cbe17c
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-08 22:34:23 +0100

    :lemon: Fix formatting

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -4,8 +4,8 @@
 
 Why does this file exist, and why ``__main__``? For more info, read:
 
- - https://www.python.org/dev/peps/pep-0338/
- - https://docs.python.org/3/using/cmdline.html#cmdoption-m
+- https://www.python.org/dev/peps/pep-0338/
+- https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 
 from .cli import main
