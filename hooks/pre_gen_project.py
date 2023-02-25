f1f89a8d67f24ab45922076794e1544018a672f8
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-02-25 18:00:02 +0100

    Blacken hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,13 +8,13 @@ Adapted from https://github.com/audreyfeldroy/cookiecutter-pypackage/blob/master
 import re
 import sys
 
-MODULE_REGEX = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
+MODULE_REGEX = re.compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
 
-module_name = '{{ cookiecutter.package_name}}'
+module_name = "{{ cookiecutter.package_name}}"
 
 if not MODULE_REGEX.match(module_name):
     print(
-        f'ERROR: {module_name} is not a valid Python module name. Please do not use a - and use _ instead'
+        f"ERROR: {module_name} is not a valid Python module name. Please do not use a - and use _ instead"
     )
 
     # Exit to cancel project
