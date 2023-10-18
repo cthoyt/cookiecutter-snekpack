a5493870ea0ef0a9f969ff0747ddff595f9ef399
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-18 11:14:28 +0200

    Add additional docs in api.py
    References #11

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/api.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/api.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
-"""Main code."""
+"""This is a placeholder for putting the main code for your module.
+
+If you don't want to keep ``api.py``, check the following places:
+
+1. ``src/{{ cookiecutter.package_name }}/__init__.py`` contains a line
+   ``from .api import *  # noqa``. You'll want to delete this and likely replace
+   it with other imports for the most important functionality for your package
+2. ``docs/usage.rst`` contains a line
+   ``.. automodule:: {{cookiecutter.package_name}}.api``.
+   You'll want to delete this and replace it with other imports
+3. You don't need to update any linting or testing configuration since it is agnostic
+   to the contents of the package.
+"""
