af42aed23fa293b51044cc758076ba9b457088f0
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-05-08 11:29:20 +0200

    Update ignore for running git subprocess
    apparently it's not in the right spot anymore

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/version.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/version.py
@@ -21,8 +21,8 @@ def get_git_hash() -> str:
     """Get the :mod:`{{cookiecutter.package_name}}` git hash."""
     with open(os.devnull, "w") as devnull:
         try:
-            ret = check_output(  # noqa: S603,S607
-                ["git", "rev-parse", "HEAD"],
+            ret = check_output(  # noqa: S603
+                ["git", "rev-parse", "HEAD"],  # noqa: S607
                 cwd=os.path.dirname(__file__),
                 stderr=devnull,
             )
