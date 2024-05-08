56485f91538296bfe7ccb09858050a86f2e78faa
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-05-08 11:29:27 +0200

    Fix line length in cli.py

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
@@ -2,11 +2,13 @@
 
 """Command line interface for :mod:`{{cookiecutter.package_name}}`.
 
-Why does this file exist, and why not put this in ``__main__``? You might be tempted to import things from ``__main__``
+Why does this file exist, and why not put this in ``__main__``?
+You might be tempted to import things from ``__main__``
 later, but that will cause problems--the code will get executed twice:
 
-- When you run ``python3 -m {{cookiecutter.package_name}}`` python will execute``__main__.py`` as a script.
-  That means there won't be any ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
+- When you run ``python3 -m {{cookiecutter.package_name}}`` python will
+  execute``__main__.py`` as a script. That means there won't be any
+  ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
 - When you import __main__ it will get executed again (as a module) because
   there's no ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
 
