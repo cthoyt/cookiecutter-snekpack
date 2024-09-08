3f78a8dd30c55235b114c20a768db94d88a12037
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-09-08 11:25:43 +0200

    Update cli.py

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
@@ -22,7 +22,7 @@ __all__ = [
 
 @click.command()
 @click.option("--name", required=True, help="The name of the person to say hello to")
-def main(name):
+def main(name: str) -> None:
     """CLI for {{cookiecutter.package_name}}."""
     # import inside the CLI to make running the --help command faster
     from .api import hello
