2bcd26d7116bc302fbda181db4a99df6903541bb
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-10-28 11:22:27 +0100

    Make exports explicit

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,3 +1,9 @@
 """{{ cookiecutter.short_description.rstrip(".") }}."""
 
-from .api import *  # noqa
+from .api import hello, square
+
+# being explicit about exports is important!
+__all__ = [
+    "hello",
+    "square",
+]
